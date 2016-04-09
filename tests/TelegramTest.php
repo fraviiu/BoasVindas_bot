1ed40a562e9f7c258014fc71171112b32928b94d
João Vítor Dias <joaodias@noctus.org>
2016-04-09 18:08:02 -0300

    Update TelegramTest.php

--- tests/TelegramTest.php
+++ tests/TelegramTest.php
@@ -1,19 +1,34 @@
 <?php
 
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
 use Telegram\Bot\Api;
+use Telegram\Bot\Objects\Message;
 
 class TelegramTest extends TestCase
 {
-    protected $telegram;
+	protected $telegram;
 
-    public function setUp()
-    {
-        $this->telegram = new Api('token');
-    }
+	public function setUp()
+	{
+		$this->telegram = new Api('token');
+	}
 
-    /** @test */
-    public function token_ok()
-    {
-        new Api(env('TELEGRAM_BOT_TOKEN'));
-    }
+	/** @test */
+	public function sendMessage()
+	{
+		$this->telegram = new Api(env('TELEGRAM_BOT_TOKEN', ''));
+
+		$var = [
+			'chat_id'	=> env('TELEGRAM_CHAT_ID', ''),
+			'text'		=> 'OK'
+		];
+
+		$response = $this->telegram->sendMessage(['chat_id' => $var['chat_id'], 'text' => $var['text']]);
+
+		$this->assertInstanceOf(Message::class, $response);
+		$this->assertEquals($var['chat_id'], $response->getChat()->getId());
+		$this->assertEquals($var['text'], $response->getText());
+	}
 }
